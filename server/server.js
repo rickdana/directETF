3235c126732b56a47761a5d1a6f675502f9fe05e
demsking <demsking@gmail.com>
2015-10-28 17:10:49 +0100

    - use strict mode - comment code

--- server/server.js
+++ server/server.js
@@ -1,29 +1,53 @@
+'strict mode';
+
 var express = require('express')
   , serveStatic = require('serve-static')
   , url = require('url')
   , path = require('path');
 
-var app = express()
-  , env = process.env.NODE_ENV || "production"
+var app = express();
+
+// Load env config
+var env = process.env.NODE_ENV || "dev"
   , config;
 
-if (env == "production") {
-  config = require('./app-aws.json');
-} else {
-  config = require('./app-dev.json');
+try {
+  config = require('./app-' + env + '.json');
+} catch (e) {
+  console.error("No such config file for env '%s'!", env);
+  process.exit(1);
 }
 
-app.get(/^\/(app.js|app-aws\.json|app-dev\.json)$/i, function (req, res) {
-  res.status(404);
-  res.send("Not Found");
+// Log in route
+app.post('/login',
+  //passport.authenticate('local'),
+  function(req, res) {
+    // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+    //console.log(req.user.username)
+    res.redirect('/dashboard/');
+  });
+
+//app.post('/login',
+//  passport.authenticate('local', { successRedirect: '/dashboard',
+//                                   failureRedirect: '/login',
+//                                   failureFlash: true }));
+
+// Log Out route
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
 });
 
+// Serve static files on /app
 app.use(serveStatic(path.join(process.cwd(), '/app')));
 
+// Specific route for client to get address of ws's host
 app.get('/config/ws/host', function (req, res) {
   res.send(config.WS_URL);
 });
 
+// Route of *.html
 app.get(/^\/.+\.html$/, function (req, res) {
   var url_parsed = url.parse(req.url, true);
   var pathname = url_parsed.pathname;
@@ -35,6 +59,7 @@ app.get(/^\/.+\.html$/, function (req, res) {
   res.sendFile(__dirname + pathname);
 });
 
+// Launch express server
 var server = app.listen(config.EXPRESS_PORT, function () {
   var host = server.address().address;
   var port = server.address().port;
