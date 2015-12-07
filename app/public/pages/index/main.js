f2e58f244779cec08202fb1d580ad27e09c7bec5
nbajard120115 <ninon.bajard@sgcib.com>
2015-12-07 17:07:01 +0100

    ajout de animate (about service contact login + petite fleche +logos bien styles). Modification des logos et de la photo iphone (contraste) + changement de couleur texte footer + changement de couleur boutons (hover)

--- app/public/pages/index/main.js
+++ app/public/pages/index/main.js
@@ -5,3 +5,24 @@ angular.module('myApp', ['ngRoute']).
 config(['$routeProvider', function($routeProvider) {
   //$routeProvider.otherwise({redirectTo: '/view1'});
 }]);
+
+$(document).ready(function() {
+    $('.navbar-nav > li > a').on('mouseover', function() {
+        $(this).addClass('animated flash');
+    });
+
+    $('.navbar-nav > li > a').on('mouseout', function() {
+        $(this).removeClass('animated flash');
+    });
+
+
+    $('.fa-4x').removeClass('wow bounceIn');
+
+    $('.fa-4x').on('mouseover', function(){
+        $(this).addClass('animated bounce');
+    });
+
+    $('.fa-4x').on('mouseout', function(){
+        $(this).removeClass('animated bounce');
+    });
+})
