4b10d53548ba0294b050caf80ad0a001b459726b
demsking <demsking@gmail.com>
2016-01-04 11:34:26 +0100

    Load the CSS file, remove unused code and change the root attributes

--- app/protected/component/Questionnaire/main.js
+++ app/protected/component/Questionnaire/main.js
@@ -1,133 +1,15 @@
 angular.module('MetronicApp')
-    .factory('$OrdersFactory', function() {
-        var etfs = {};
-        var quantities = {};
-        var locked = false;
-
-        return {
-            lock: function() {
-                locked = true;
-            },
-            unlock: function() {
-                locked = false;
-            },
-            set: function(etf, quantity) {
-                if (locked) {
-                    return;
-                }
-                if (!etfs[etf.isin]) {
-                    etfs[etf.isin] = etf;
-                    quantities[etf.isin] = etf.quantity;
-
-                    return console.log("-> %s(quantity: %s, price: %s) was added in the selection list", etf.isin, etf.quantity, etf.price);
-                }
-
-                if (typeof quantity != 'undefined') {
-                    quantities[etf.isin] = parseInt(quantity);
-                } else {
-                    quantity = quantities[etf.isin];
-                }
-
-                console.log("-- Set %s(quantity: %s, price: %s)", etf.isin, quantity, etf.price);
-            },
-            get: function(isin) {
-                if (isin) {
-                    if (etfs[isin]) {
-                        return etfs[isin];
-                    }
-                    return null;
-                }
-
-                var array = [];
-
-                for (var isin in etfs) {
-                    if (etfs[isin] && quantities[isin]) {
-                        etfs[isin].quantity = quantities[isin];
-                        array.push(etfs[isin]);
-                    }
-                }
-
-                return array;
-            },
-            length: function() {
-                var i = 0;
-
-                for (var isin in etfs) {
-                    if (quantities[isin]) {
-                        i++;
-                    }
-                }
-
-                return i;
-            },
-            cash: function() {
-                var cash = 0;
-
-                for (var isin in quantities) {
-                    cash += quantities[isin] * etfs[isin].price;
-                }
-
-                return cash;
-            },
-            distribution: function($etfs, limit) {
-                if (!$etfs || !$etfs.length) {
-                    return;
-                }
-
-                var initial_cash = 0;
-                var percents = {};
-                var ratio = limit / initial_cash;
-
-                for (var i in $etfs) {
-                    initial_cash += $etfs[i].price;
-                }
-
-                for (var i in $etfs) {
-                    percents[$etfs[i].isin] = $etfs[i].price / initial_cash;
-                }
-
-                this.unlock();
-
-                for (var i in $etfs) {
-                    var isin = $etfs[i].isin;
-                    var quantity = Math.floor(percents[isin] * limit / etfs[isin].price);
-
-                    $etfs[i].quantity = quantity;
-                    this.set($etfs[i], quantity);
-                }
-
-                var diff = limit - this.cash();
-
-                while (diff > 0) {
-                    for (var i = 0, j = 0; i < $etfs.length; i++) {
-                        if ($etfs[i].price <= diff) {
-                            this.set($etfs[i], ++$etfs[i].quantity);
-                            diff -= $etfs[i].price;
-
-                            if (diff <= 0) {
-                                j = i;
-                                break;
-                            }
-                        } else {
-                            j++;
-                        }
-                    }
-
-                    if (i == j) {
-                        break;
-                    }
-                }
-
-                this.lock();
-            }
-        };
-    })
-    .controller('QuestionnaireController', function($OrdersFactory, $scope, $http, $attrs) {
+    .controller('QuestionnaireController', function($ocLazyLoad, $scope, $http, $attrs) {
         if ($attrs.json) {
+            $ocLazyLoad.load({
+                insertBefore: '#ng_load_plugins_before',
+                files: [
+                    '/protected/component/Questionnaire/style.css',
+                ]
+            });
+
             $http.get($attrs.json)
                 .then(function(questionnaireFile) {
-                    $scope.$OrdersFactory = $OrdersFactory;
-
                     $scope.filters = {
                         current: {
                             category: questionnaireFile.data.start,
