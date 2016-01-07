cbd79f166f5abfcd6e823860aa1c8a2931a331f2
yangruoxi <ruoxi.yang@utbm.fr>
2016-01-07 15:05:11 +0100

    Ajouter highStock dans la conclusion du premier questionaire

--- app/public/pages/essai/main.js
+++ app/public/pages/essai/main.js
@@ -1,7 +1,8 @@
 'use strict';
 
 angular.module('DirectETF', [])
-    .controller('EssaiController', function($scope, $element) {
+
+    .controller('EssaiController', function($scope, $element, $http) {
         $('body').addClass('overlay');
 
 
@@ -83,6 +84,7 @@ angular.module('DirectETF', [])
         };
 
         $scope.step = 1;
+        $scope.limit = limit;
 
         $scope.prev = function() {
             if ($scope.step > 1) {
@@ -109,4 +111,217 @@ angular.module('DirectETF', [])
                 }
             }
         };
+
+        $scope.montant = 0;
+
+        $scope.model = {
+            risque: null,
+            frequence: null,
+        };
+
+        $scope.synthese = function() {
+            var done = function (etfs) {
+                var etfs_infos = [],
+                    montant = $scope.montant;
+
+                for(var i in etfs) {
+                    var quantity = Math.floor (montant * etfs[i].percent / 100 / etfs[i].price);
+                    etfs_infos.push([etfs[i].isin, quantity, etfs[i].price, etfs[i].profitability, etfs[i].volatility]);
+                }
+
+                draw_simulation_future(etfs_infos);
+            };
+
+            var risk = $scope.model.riskLevel > 50 ? 'high' : 'low';
+
+            $http.get('http://184.51.43.30:8080/portfolio/model/home/500/' + risk)
+                .success(function (model, status, headers, config) {
+                    var etfs = [];
+
+                    for (var i = 0; i < model.composition.length; i++) {
+                        var isin = model.composition[i][0];
+                        var percent = model.composition[i][1];
+
+                        $http.get('http://184.51.43.30:8080/etf/desc/' + isin)
+                            .success(function (desc, status, headers, config) {
+                                desc.percent = percent;
+
+                                $http.get('http://184.51.43.30:8080/etf/price/' + isin)
+                                    .success(function (__data) {
+                                        desc.price = 0;
+
+                                        for (var __p in __data) {
+                                            desc.price = __data[__p] || 0;
+                                        }
+
+                                        etfs.push(desc);
+
+                                        if (etfs.length == model.composition.length) {
+                                            // end of list
+                                            done(etfs);
+                                        }
+                                    })
+                                    .error(function(data, status, headers, config) {
+                                        desc.price = 0;
+
+                                        etfs.push(desc);
+
+                                        if (etfs.length == model.composition.length) {
+                                            // end of list
+                                            done(etfs);
+                                        }
+
+                                        throw new Error("Failed to get price of ETF " + isin);
+                                    });
+                            })
+                            .error(function(data, status, headers, config) {
+                                console.error("Failed to get description of ETF %s", isin);
+                            });
+                    }
+
+                    //console.log(model.composition)
+                })
+                .error(function(data, status, headers, config) {
+                    var err = new Error("Failed to get model of the risk " + riskLevel);
+
+                    err.status = status;
+                    err.headers = headers;
+
+
+                    console.error(err.message);
+                });
+        };
+
+        //get the date of next month
+        function next_month(date) {
+            var date_1 = new Date(date);
+            var month = date_1.getMonth() + 1;
+            var year = date_1.getFullYear();
+            if (month != 12) {
+                month += 1;
+            } else {
+                year += 1;
+                month = 1;
+            }
+
+
+            if (month < 10) {
+                return new Date(year + '-0' + month + '-01');
+            } else {
+                return new Date(year + '-' + month + '-01');
+            }
+        }
+
+        //format utc to yyyy/mm/dd
+        function formatDate(date) {
+            var d = new Date(date),
+                month = '' + (d.getMonth() + 1),
+                day = '' + d.getDate(),
+                year = d.getFullYear();
+
+            if (month.length < 2) month = '0' + month;
+            if (day.length < 2) day = '0' + day;
+
+            return [year, month, day].join('-');
+        }
+
+        function simulation_future(ref_etfs, time_frame, data_valo_today, left_vol, right_vol) {
+            var simulation_future_etfs = {};
+            var simulation_future_etfs_moins_vola = {};
+            var simulation_future_etfs_ajoute_vola = {};
+            var data_simu_future = [];
+
+            var firstDay = formatDate(data_valo_today);
+
+            //ref-etfs = [isin, qdt, price, rentabilite, volatilire]
+            for (var i in ref_etfs) {
+                var taux_rentabilite = ref_etfs[i][3];
+                var value_etf = ref_etfs[i][1] * ref_etfs[i][2];
+                var month = firstDay;
+                var left_volatilite = ref_etfs[i][4] * left_vol * ref_etfs[i][1];
+                var right_volatilite = ref_etfs[i][4] * right_vol * ref_etfs[i][1];
+
+                simulation_future_etfs[month] = value_etf;
+                if(typeof simulation_future_etfs_moins_vola[month] == 'undefined') {
+                    simulation_future_etfs_moins_vola[month] = simulation_future_etfs[month];
+                    simulation_future_etfs_ajoute_vola[month] = simulation_future_etfs[month];
+                } else {
+                    simulation_future_etfs_moins_vola[month] += simulation_future_etfs[month];
+                    simulation_future_etfs_ajoute_vola[month] += simulation_future_etfs[month];
+                }
+
+                for (var i = 0; i < time_frame; i++) {
+                    for (var j = 1; j <= 12; j++) {
+                        month = next_month(month);
+                        simulation_future_etfs[month] = value_etf * (taux_rentabilite / 12 * j + 1);
+                        if(typeof simulation_future_etfs_moins_vola[month] == 'undefined') {
+                            simulation_future_etfs_moins_vola[month] = simulation_future_etfs[month] + left_volatilite;
+                            simulation_future_etfs_ajoute_vola[month] = simulation_future_etfs[month] + right_volatilite;
+                        } else {
+                            simulation_future_etfs_moins_vola[month] += simulation_future_etfs[month] + left_volatilite;
+                            simulation_future_etfs_ajoute_vola[month] += simulation_future_etfs[month] + right_volatilite;
+                        }
+                    }
+                    value_etf *= (1 + taux_rentabilite);
+                }
+            }
+            for (var date in simulation_future_etfs) {
+                var low = parseFloat(simulation_future_etfs_moins_vola[date].toFixed(2));
+                var high = parseFloat(simulation_future_etfs_ajoute_vola[date].toFixed(2));
+                data_simu_future.push([new Date(date).getTime(), low, high]);
+            }
+
+            data_simu_future.sort(function (a, b) {
+                return a[0] - b[0];
+            });
+
+            return data_simu_future;
+
+        }
+
+        function draw_simulation_future(ref_etfs_new_invests) {
+            //simulation-graph of the future
+
+            var data_valo_today = new Date().getTime();
+
+            var data_invest_future_attendu = simulation_future(ref_etfs_new_invests, 10, data_valo_today, -1, 1);
+            var data_invest_future_favorable = simulation_future(ref_etfs_new_invests, 10, data_valo_today, 1, 2);
+            var data_invest_future_defavorable = simulation_future(ref_etfs_new_invests, 10, data_valo_today, -2, -1);
+
+
+            //simulation-graph of the future
+            var series = [{
+                name: 'Prévision - favorable 13%',
+                id: 'Prévision_2',
+                type: 'arearange',
+                data: data_invest_future_favorable,
+                color:'rgb(32, 121, 57)',
+                zIndex: 11,
+                threshold: null,
+                showInLegend: false,
+            },{
+                name: 'Prévision - 68%',
+                id: 'Prévision_1',
+                type: 'arearange',
+                data: data_invest_future_attendu,
+                color:  'rgb(43, 161, 76)' ,
+                zIndex: 11,
+                threshold: null,
+                showCheckbox: true,
+                showInLegend: false
+            },{
+                name: 'Prévision - defavorable 13%',
+                id: 'Prévision_3',
+                type: 'arearange',
+                data: data_invest_future_defavorable,
+                color: 'rgb(255, 198, 179)',
+                zIndex: 11,
+                threshold: null,
+                showInLegend: false
+            }];
+
+            LoadStockChart(series, $('#questionaire-future-stockchart'), true);
+
+            //$('#simulation-future').highcharts().legend.allItems[0].update({name:'Prévision sans nouveaux investissements'});
+        }
     });
