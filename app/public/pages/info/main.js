00dfa2a59708c9c529ddcf27893dd7b9986fef56
yangruoxi <ruoxi.yang@utbm.fr>
2016-01-20 14:18:23 +0100

    Remove the tooltip on the slider

--- app/public/pages/info/main.js
+++ app/public/pages/info/main.js
@@ -1,14 +1,23 @@
 'use strict';
 
 angular.module('DirectETF', [])
-    .controller('InfoController', function($element) {
-
-
-        $($element[0]).find('.appear').each(function() {
+    .controller('InfoController', function ($scope, $element) {
+        $($element[0]).find('.appear').each(function () {
             $($(this).get(0)).bind('appear', function () {
                 $(this).addClass($(this).attr('data-animation'));
             });
         });
 
-
+        $scope.slider = {
+            options: {
+                floor: 100,
+                ceil: 100000,
+                interval: 100,
+                hideLimitLabels: true,
+                showSelectionBar: true,
+                translate: function () {
+                    return '';
+                }
+            }
+        };
     });
