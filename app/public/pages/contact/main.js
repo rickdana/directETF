29cb83421296a12ad1a19deb4c0885476e8b3781
demsking <demsking@gmail.com>
2016-01-25 17:42:56 +0100

    first contact page structure

--- app/public/pages/contact/main.js
+++ app/public/pages/contact/main.js
@@ -2,5 +2,14 @@
 
 angular.module('DirectETF', [])
     .controller('ContactController', function ($scope, $element) {
+        $scope.model = {
+            name: '',
+            email: '',
+            subject: '',
+            message: '',
+        };
 
+        $scope.send = function() {
+
+        };
     });
